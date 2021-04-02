4c5452819a3a86225e151a586a31add60786c230
Wagner Ribeiro <wagnerras@gmail.com>
2021-04-02 11:24:21 -0300

    wag

--- api/offer.js
+++ api/offer.js
@@ -33,6 +33,19 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
+    async function usersGet(req, res) {
+
+        await setState()
+
+        app.db('offers')
+            .select('advertiser_name', 'description', 'url')
+            .where({ state: 'enabled' })
+            .orderBy('premium', 'desc')
+            .then(offers => res.json(offers))
+            .catch(err => res.status(500).send(err))
+
+    }
+
     function getById(req, res) {
         app.db('offers')
             .where({ id: req.params.id })
@@ -104,7 +117,6 @@ module.exports = app => {
     }
 
 
-
     function validURL(str) {
         regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
@@ -142,7 +154,5 @@ module.exports = app => {
     }
 
 
-
-
-    return { save, get, getById, update, remove, setState, disable }
+    return { save, get, getById, update, remove, setState, disable, usersGet }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,21 @@
 module.exports = app => {
 
-    app.route('/offers')
+    app.route('/adm/offers')
         .post(app.api.offer.save)
         .get(app.api.offer.get)
 
-    app.route('/offers/:id')
+    app.route('/adm/offers/:id')
         .get(app.api.offer.getById)
         .put(app.api.offer.update)
         .delete(app.api.offer.remove)
 
-    app.route('/offers/:id/disable')
+    app.route('/adm/offers/:id/disable')
         .put(app.api.offer.disable)
 
-    app.route('/setState')
-        .get(app.api.offer.setState)
+
+    app.route('/users/offers')
+        .get(app.api.offer.usersGet)
+
 
 
 
